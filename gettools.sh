88cdec06788a916e4464a383c98081ad44332436
C_Sto <cameronrstokes@outlook.com>
2017-04-18 22:14:47 +0800

    fix docker

--- gettools.sh
+++ gettools.sh
@@ -153,11 +153,11 @@ git clone https://github.com/adaptivethreat/Bloodhound
 # THIS SEEMS OK
 
 #docker :rolleyes: adsfasdfsadf no idea
-#apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-#apt-get update
-#apt-get -y install docker-ce
+echo 'deb http://http.debian.net/debian wheezy-backports main' > /etc/apt/sources.list.d/backports.list && apt-get update
+apt-get -y install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb https://apt.dockerproject.org/repo debian-wheezy main' > /etc/apt/sources.list.d/docker.list && apt-get update
+apt-get -y install docker-engine docker && service docker start
 
 # vbox
 apt-get -y install virtualbox
